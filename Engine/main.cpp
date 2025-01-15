e50a75e7d5720da2b7d4449ffb47bc9e83e6a9d9
Jorge Bea Frau <jbeafrau@uoc.edu>
2025-01-15 21:40:58 +0100

    Commented main file

--- Engine/main.cpp
+++ Engine/main.cpp
@@ -20,43 +20,60 @@ game Game;
 //Enable for debugging
 //Game.debugMode = true;
 
+//Init SDL subsystems
 if (Game.initSDL() == false) {
     Game.setState(my_enums::S_GAMEOVER_);
 }
 
+//Load media (images/audio)
 Game.loadMedia(Game.getBasePath());
 
 Game.start();
 
 while (Game.getState() != my_enums::S_GAMECLOSE_){
     
+    //Process input events for current state
     Game.events();
 
+    //clear screen
     Game.screenClear();
 
+    //draw background 
     Game.drawBackground();
     
+
+    //draw screen for current state
     Game.drawScreens();
 
+    //update and play ongoing animations
     Game.playAnimations();
 
+    //Updates notifications
     Game.eventsNotifications();
 
+    //Draw notifications
     Game.screenNotifications();
 
+    //Draw achievements
     Game.drawAchievements();
 
+    //Darw mouse location
     Game.drawMouse();
 
+    //Flip generated frame on screen
     Game.screenFlip();
 
+    //Process events related to current player location
     Game.locationEvents();
 
+    //Process global events
     Game.timeEvents();
 
+    //Adjust frame rate
     Game.adjustFPS();
 }
 
+//Close SDL subsystems
 Game.closeSDL();
 
 return 0;
